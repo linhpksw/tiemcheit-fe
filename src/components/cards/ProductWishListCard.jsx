6e8a8041dd46fca2aed77158bf504d3f966034e4
thangnt <tatthang283@gmail.com>
2024-06-06 00:07:04 +0700

    feat: show wishlist, add to wishlist and delete from wishlist

--- src/components/cards/ProductWishListCard.jsx
+++ src/components/cards/ProductWishListCard.jsx
@@ -11,29 +11,30 @@ const ProductWishlistToggler = dynamic(
 );
 
 const ProductWishListCard = ({ dish }) => {
-  const { images, name, price, review, restaurant_id, restaurant } = dish;
+  const { images, name, price, review, restaurant_id, restaurant, product } =
+    dish;
 
-  const discountedPrice = calculatedPrice(dish);
+  // const discountedPrice = calculatedPrice(dish);
 
   return (
     <div className="grid grid-cols-3 items-end px-4 py-4 sm:items-center">
       <div className="col-span-2 lg:col-span-1">
         <div className="flex flex-col sm:flex-row sm:items-center">
           <Image
-            src={images[0]}
+            src={images}
             width={112}
             height={112}
             className="me-2 h-28 w-28 place-self-end sm:place-self-start lg:me-4"
-            alt="sushi"
+            alt="img"
           />
           <div className="w-2/3 md:w-auto">
             <p className="mb-2 text-xs font-medium text-primary">
               {restaurant?.name ?? ""}
             </p>
             <h4 className="mb-2 line-clamp-1 text-xl font-semibold text-default-800">
-              {name}
+              {product.name}
             </h4>
-            <div className="flex items-center gap-2">
+            {/* <div className="flex items-center gap-2">
               <div className="flex items-center gap-1.5">
                 <span className="inline-flex items-center gap-2 lg:hidden">
                   <span className="rounded-full bg-primary p-1">
@@ -42,8 +43,8 @@ const ProductWishListCard = ({ dish }) => {
                   <span className="from-inherit text-sm text-default-950">
                     {review.stars}
                   </span>
-                </span>
-                <div className="hidden gap-1.5 lg:flex">
+                </span> */}
+            {/* <div className="hidden gap-1.5 lg:flex">
                   {Array.from(new Array(Math.floor(review.stars))).map(
                     (_star, idx) => (
                       <FaStar
@@ -66,12 +67,12 @@ const ProductWishListCard = ({ dish }) => {
                         />
                       )
                     )}
-                </div>
-              </div>
+                </div> */}
+            {/* </div>
               <h6 className="mt-1 text-sm font-medium text-default-500">
                 ({review.count})
               </h6>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
@@ -79,18 +80,18 @@ const ProductWishListCard = ({ dish }) => {
         <div className="flex flex-col items-end lg:text-end">
           <h4 className="inline-block text-2xl font-medium text-default-500">
             {currentCurrency}
-            {discountedPrice}
+            {product.price}
           </h4>
-          <h4 className="inline-block text-base font-medium text-default-500 line-through">
+          {/* <h4 className="inline-block text-base font-medium text-default-500 line-through">
             {currentCurrency}
-            {price}
-          </h4>
+            {product.price}
+          </h4> */}
         </div>
       </div>
       <div className="col-span-3 mt-4 lg:col-span-1 lg:mt-0">
         <div className="flex justify-center gap-5 lg:flex-col lg:items-end">
-          <ProductWishlistToggler dish={dish} />
-          <AddToCartButton dish={dish} />
+          <ProductWishlistToggler dish={product} />
+          <AddToCartButton dish={product} />
         </div>
       </div>
     </div>
