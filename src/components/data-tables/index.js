421955650b22ebb9e4f1c1e60643588ac5ad4fc4
Trọng Linh <linhpksw@gmail.com>
2024-06-25 17:18:35 +0700

    feat: implement interface for system log

--- src/components/data-tables/index.js
+++ src/components/data-tables/index.js
@@ -13,3 +13,4 @@ export { default as RestaurantMenuDataTable } from './RestaurantMenuDataTable';
 export { default as InactiveProductDetailView } from './InactiveProductDetailView';
 export { default as DisableProductDetailView } from './DisableProductDetailView';
 export { default as CouponDataTable } from './CouponDataTable';
+export { default as LogDataTable } from './LogDataTable';
