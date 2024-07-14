aa06c1fdfad945cda7918e889f57084f8ba228b9
Trọng Linh <linhpksw@gmail.com>
2024-07-14 13:54:36 +0700

    refactor: removed unnecessary files

--- src/app/[username]/(ingredient)/ingredients/[IngredientId]/EditForm.jsx
+++ src/app/[username]/(ingredient)/ingredients/[IngredientId]/EditForm.jsx
@@ -1,40 +1,39 @@
-// EditForm.jsx
 "use client";
 import { TextFormInput } from "@/components";
 
 const EditForm = ({ control, handleSubmit, onSubmit }) => {
-	return (
-		<div className="xl:col-span-2">
-			<div className="rounded-lg border border-default-200 p-6">
-				<div className="space-y-6">
-					<TextFormInput
-						name="ingredientName"
-						type="text"
-						label="Tên nguyên liệu"
-						placeholder="Tên nguyên liệu"
-						control={control}
-						fullWidth
-					/>
-					<TextFormInput
-						name="price"
-						type="number"
-						label="Giá"
-						placeholder="Giá"
-						control={control}
-						fullWidth
-					/>
-					<TextFormInput
-						name="quantity"
-						type="number"
-						label="Số lượng(gram)"
-						placeholder="Số lượng"
-						control={control}
-						fullWidth
-					/>
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className="xl:col-span-2">
+            <div className="rounded-lg border border-default-200 p-6">
+                <div className="space-y-6">
+                    <TextFormInput
+                        name="ingredientName"
+                        type="text"
+                        label="Tên nguyên liệu"
+                        placeholder="Tên nguyên liệu"
+                        control={control}
+                        fullWidth
+                    />
+                    <TextFormInput
+                        name="price"
+                        type="number"
+                        label="Giá"
+                        placeholder="Giá"
+                        control={control}
+                        fullWidth
+                    />
+                    <TextFormInput
+                        name="quantity"
+                        type="number"
+                        label="Số lượng(gram)"
+                        placeholder="Số lượng"
+                        control={control}
+                        fullWidth
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default EditForm;
