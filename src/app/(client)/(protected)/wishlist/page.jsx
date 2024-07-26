52d648ba48188c4bd2597bf5918a3c5c04b5f609
thangnt <tatthang283@gmail.com>
2024-07-26 16:26:11 +0700

    fix: handles active/inactive dishes

--- src/app/(client)/(protected)/wishlist/page.jsx
+++ src/app/(client)/(protected)/wishlist/page.jsx
@@ -11,6 +11,7 @@ const Wishlist = () => {
 			<Breadcrumb title="Danh sách yêu thích" />
 			<section className="py-6 lg:py-10">
 				<div className="container">
+					<div className="text-xl font-medium mb-2">Danh sách yêu thích</div>
 					<div className="divide-y divide-default-200 overflow-hidden rounded-lg border border-default-200 ">
 						<AllWishList />
 					</div>
