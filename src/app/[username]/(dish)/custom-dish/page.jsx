316edc736ce3e3d1241a87ffc41d9bfd8cbad349
nkl04 <longnguyenkim2707@gmail.com>
2024-07-28 01:51:50 +0700

    update:  optionList

--- src/app/[username]/(dish)/custom-dish/page.jsx
+++ src/app/[username]/(dish)/custom-dish/page.jsx
@@ -1,37 +1,34 @@
-"use client";
-import { useState, useEffect } from "react";
-import AddDishForm from "./AddDishForm";
-import { BreadcrumbAdmin } from "@/components";
-import { useParams } from "next/navigation";
-import { useUser } from "@/hooks";
-import { LuEraser, LuPlus, LuSave } from "react-icons/lu";
-import { useForm } from "react-hook-form";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { addProduct, fetchProductById } from "@/helpers";
-import { useShoppingContext } from "@/context";
-import { getProductDetailByIdWithOutAT } from "@/helpers";
+'use client';
+import { useState, useEffect } from 'react';
+import AddDishForm from './AddDishForm';
+import { BreadcrumbAdmin } from '@/components';
+import { useParams } from 'next/navigation';
+import { useUser } from '@/hooks';
+import { LuEraser, LuPlus, LuSave } from 'react-icons/lu';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { addProduct, fetchProductById } from '@/helpers';
+import { useShoppingContext } from '@/context';
+import { getProductDetailByIdWithOutAT } from '@/helpers';
 
 const credentialsManagementFormSchema = yup.object({
-	productname: yup.string().required("Vui lòng nhập tên sản phẩm của bạn"),
-	quantity: yup
-		.number()
-		.typeError("Nhập sai định dạng")
-		.required("Vui lòng nhập số lượng của bạn"),
-	description: yup.string().required("Vui lòng nhập mô tả của bạn"),
-	ingredients: yup.string().required("Phải chọn ít nhất một nguyên liệu"),
+	productname: yup.string().required('Vui lòng nhập tên sản phẩm của bạn'),
+	quantity: yup.number().typeError('Nhập sai định dạng').required('Vui lòng nhập số lượng của bạn'),
+	description: yup.string().required('Vui lòng nhập mô tả của bạn'),
+	ingredients: yup.string().required('Phải chọn ít nhất một nguyên liệu'),
 });
 
 const formData = {
-	name: "",
+	name: '',
 	price: 0,
 	quantity: 0,
-	description: "",
+	description: '',
 	category: {},
 	imageList: [],
-	status: "",
-	createAt: "",
-	optionId: [],
+	status: '',
+	createAt: '',
+	optionList: [],
 	productIngredients: [],
 };
 
@@ -43,8 +40,7 @@ const AddProduct = () => {
 	const [productId, setProductId] = useState(null);
 
 	const [totalPrice, setTotalPrice] = useState(0);
-	const { addToCart, removeFromCart, isInCart, cartItems } =
-		useShoppingContext();
+	const { addToCart, removeFromCart, isInCart, cartItems } = useShoppingContext();
 	const { control, handleSubmit, reset } = useForm({
 		resolver: yupResolver(credentialsManagementFormSchema),
 	});
@@ -58,21 +54,15 @@ const AddProduct = () => {
 
 	const handleIngredientChange = (ingredients) => {
 		setSelectedIngredients(ingredients);
-		const price = ingredients.reduce(
-			(sum, ingredient) => sum + ingredient.price * ingredient.quantity,
-			0
-		);
+		const price = ingredients.reduce((sum, ingredient) => sum + ingredient.price * ingredient.quantity, 0);
 		setTotalPrice(price);
 	};
 
 	const onSubmit = async (data) => {
 		try {
-			const totalUIC = selectedIngredients.reduce(
-				(sum, ingredient) => sum + ingredient.quantity,
-				0
-			);
+			const totalUIC = selectedIngredients.reduce((sum, ingredient) => sum + ingredient.quantity, 0);
 			if (totalUIC < 3 || totalUIC > 5) {
-				alert("Món ăn phải có 3, 4 hoặc 5 UIC.");
+				alert('Món ăn phải có 3, 4 hoặc 5 UIC.');
 				return;
 			}
 			formData.name = data.productname;
@@ -81,8 +71,8 @@ const AddProduct = () => {
 			formData.description = data.description;
 			formData.category = { id: 1 };
 			formData.createAt = new Date().toISOString();
-			formData.imageList = ["defaultDish.jpg"];
-			formData.status = "custom";
+			formData.imageList = ['defaultDish.jpg'];
+			formData.status = 'custom';
 			formData.productIngredients = selectedIngredients.map((ingredient) => ({
 				ingredient: { id: ingredient.id },
 				unit: ingredient.quantity,
@@ -90,26 +80,26 @@ const AddProduct = () => {
 
 			const response = await addProduct(formData);
 			setProductId(response.id); // Assuming the response contains the new product ID
-			alert("Thêm sản phẩm thành công");
+			alert('Thêm sản phẩm thành công');
 			handleReset();
 		} catch (error) {
 			console.error(error);
-			alert("Thêm sản phẩm thất bại");
+			alert('Thêm sản phẩm thất bại');
 		}
 	};
 
 	const addCustomedProductToCart = async () => {
 		try {
 			if (!productId) {
-				alert("Vui lòng lưu sản phẩm trước khi thêm vào giỏ hàng");
+				alert('Vui lòng lưu sản phẩm trước khi thêm vào giỏ hàng');
 				return;
 			}
 			const productFetched = await getProductDetailByIdWithOutAT(productId);
 			await addToCart(productFetched, productFetched.quantity);
-			alert("Sản phẩm đã được thêm vào giỏ hàng");
+			alert('Sản phẩm đã được thêm vào giỏ hàng');
 		} catch (error) {
 			console.error(error);
-			alert("Không thể thêm sản phẩm vào giỏ hàng");
+			alert('Không thể thêm sản phẩm vào giỏ hàng');
 		}
 	};
 
@@ -118,11 +108,11 @@ const AddProduct = () => {
 	}
 
 	return (
-		<div className="w-full lg:ps-64">
-			<div className="page-content space-y-6 p-6">
-				<BreadcrumbAdmin title="Chè tự chọn" subtitle="Món ăn" />
+		<div className='w-full lg:ps-64'>
+			<div className='page-content space-y-6 p-6'>
+				<BreadcrumbAdmin title='Chè tự chọn' subtitle='Món ăn' />
 
-				<form onSubmit={handleSubmit(onSubmit)} className="m-2">
+				<form onSubmit={handleSubmit(onSubmit)} className='m-2'>
 					<AddDishForm
 						control={control}
 						handleSubmit={handleSubmit}
@@ -130,30 +120,25 @@ const AddProduct = () => {
 						selectedIngredients={selectedIngredients}
 						setSelectedIngredients={handleIngredientChange}
 					/>
-					<div className="flex items-center justify-start gap-4">
-						<div className="text-lg font-semibold">
-							Tổng giá tiền: {totalPrice} VNĐ
-						</div>
+					<div className='flex items-center justify-start gap-4'>
+						<div className='text-lg font-semibold'>Tổng giá tiền: {totalPrice} VNĐ</div>
 					</div>
-					<div className="flex items-center justify-start gap-4">
+					<div className='flex items-center justify-start gap-4'>
 						<button
-							type="button"
+							type='button'
 							onClick={handleReset}
-							className="flex items-center justify-center gap-2 rounded-lg bg-red-500/10 px-6 py-2.5 text-center text-sm font-semibold text-red-500 shadow-sm transition-colors duration-200 hover:bg-red-500 hover:text-white"
-						>
+							className='flex items-center justify-center gap-2 rounded-lg bg-red-500/10 px-6 py-2.5 text-center text-sm font-semibold text-red-500 shadow-sm transition-colors duration-200 hover:bg-red-500 hover:text-white'>
 							<LuEraser size={20} /> Reset
 						</button>
 						<button
-							type="submit"
-							className="flex items-center justify-center gap-2 rounded-lg bg-primary px-6 py-2.5 text-center text-sm font-semibold text-white shadow-sm transition-all duration-200 hover:bg-primary-500"
-						>
+							type='submit'
+							className='flex items-center justify-center gap-2 rounded-lg bg-primary px-6 py-2.5 text-center text-sm font-semibold text-white shadow-sm transition-all duration-200 hover:bg-primary-500'>
 							<LuSave size={20} /> Lưu
 						</button>
 						<button
-							type="button"
+							type='button'
 							onClick={addCustomedProductToCart}
-							className="flex items-center justify-center gap-2 rounded-lg bg-primary px-6 py-2.5 text-center text-sm font-semibold text-white shadow-sm transition-all duration-200 hover:bg-primary-500"
-						>
+							className='flex items-center justify-center gap-2 rounded-lg bg-primary px-6 py-2.5 text-center text-sm font-semibold text-white shadow-sm transition-all duration-200 hover:bg-primary-500'>
 							<LuPlus size={20} /> Thêm vào giỏ
 						</button>
 					</div>
